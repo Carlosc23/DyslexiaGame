d7a1b13dc7249fe84599aa21553e77159feb03bf
Carlos <carlos.calderon097@gmail.com>
2018-05-06 23:59:53 -0600

    feat: normal level display 5 random words
    It was added a random to display 5 different words in normal level. It will need a improvement to verify that word will not be repeated.

--- Assets/Menu_Template/Scripts/Menu_Controller.cs
+++ Assets/Menu_Template/Scripts/Menu_Controller.cs
@@ -10,7 +10,12 @@ public class Menu_Controller : MonoBehaviour {
     public string _sceneToLoadOnPlay2 = "Record";
     public string _sceneToLoadOnPlay3 = "Level";
 	public string _sceneToLoadOnPlay4 = "Level 2";
-
+    public string[] words = new string[] { "M1 Facil L", "M1 Facil L 1", "M1 Facil L 2", "M1 Facil L 3", "M1 Facil L 4",
+    "M1 Facil L 5","M1 Facil L 6","M1 Facil L 7","M1 Facil L 8","M1 Facil L 9","M1 Facil L 10","M1 Facil L 11","M1 Facil L 12",
+    "M1 Facil L 13","M1 Facil L 14","M1 Facil L 15","M1 Facil L 16","M1 Facil L 17","M1 Facil L 18","M1 Facil L 19",
+    "M1 Facil L 20","M1 Facil L 21","M1 Facil L 22","M1 Facil L 23","M1 Facil L 24","M1 Facil L 25"};
+    public static int cont = 0;
+    public static Random rnd = new Random();
     [Tooltip("_webpageURL defines the URL that will be opened when users click on your branding icon")]
 	public string _webpageURL = "http://www.alpaca.studio";
 	[Tooltip("_soundButtons define the SoundOn[0] and SoundOff[1] Button objects.")]
@@ -70,45 +75,24 @@ public class Menu_Controller : MonoBehaviour {
         _audioSource.PlayOneShot(_audioClip);
         PlayerPrefs.SetString("_LastScene", scene.name);
 		Debug.Log("es" +PlayerPrefs.GetString("_LastScene"));
-        UnityEngine.SceneManagement.SceneManager.LoadScene("M1 Facil L");
+        Debug.Log("Estoy aqui");
+        int r = Random.Range(0, 24);
+        cont++;
+        if (cont <= 5)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(words[r]);
+        }
+        else
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Level 1");
+        }
+       
     }
 	
-	public void EnterInventadasFacil2(){
-        _audioSource.PlayOneShot(_audioClip);
-        PlayerPrefs.SetString("_LastScene", scene.name);
-		Debug.Log("es" +PlayerPrefs.GetString("_LastScene"));
-        UnityEngine.SceneManagement.SceneManager.LoadScene("M1 Facil L 1");
-    }
 	
-	public void EnterInventadasFacil3(){
-        _audioSource.PlayOneShot(_audioClip);
-        PlayerPrefs.SetString("_LastScene", scene.name);
-		Debug.Log("es" +PlayerPrefs.GetString("_LastScene"));
-        UnityEngine.SceneManagement.SceneManager.LoadScene("M1 Facil L 2");
-    }
-	
-	public void EnterInventadasFacil4(){
-        _audioSource.PlayOneShot(_audioClip);
-        PlayerPrefs.SetString("_LastScene", scene.name);
-		Debug.Log("es" +PlayerPrefs.GetString("_LastScene"));
-        UnityEngine.SceneManagement.SceneManager.LoadScene("M1 Facil L 3");
-    }
-	
-	public void EnterInventadasFacil5(){
-        _audioSource.PlayOneShot(_audioClip);
-        PlayerPrefs.SetString("_LastScene", scene.name);
-		Debug.Log("es" +PlayerPrefs.GetString("_LastScene"));
-        UnityEngine.SceneManagement.SceneManager.LoadScene("M1 Facil L 4");
-    }
-	
-	public void EnterInventadasFacilRecord(){
-        _audioSource.PlayOneShot(_audioClip);
-        PlayerPrefs.SetString("_LastScene", scene.name);
-		Debug.Log("es" +PlayerPrefs.GetString("_LastScene"));
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Level 1");
-    }
 	
 	public void BackToMain(){
+        cont = 0;
         _audioSource.PlayOneShot(_audioClip);
         UnityEngine.SceneManagement.SceneManager.LoadScene("Standalone");
     }
